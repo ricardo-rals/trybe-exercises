f4b45926b34269b7a9ccef7102f37477500e275c
Rals <ricardo.rals.dev@gmail.com>
2020-08-27 19:15:19 -0300

    Resolução da questão 1 com um alert comentado

---
+++ exercises/4.1/exercise_1.js
@@ -0,0 +1,24 @@
+let A = 5;
+let B = 6;
+
+const soma = A + B;
+const subtracao = A - B;
+const multiplicacao = A * B;
+const divisao = A / B;
+const modulo = A % B;
+
+
+/*alert(
+    "Soma: " + soma + "\n" +
+    "Subtração: " + subtracao + "\n" +
+    "Multiplicação: " + multiplicacao + "\n" +
+    "Divisão: " + divisao + "\n" +
+    "Modulo: " + modulo
+
+);*/
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
