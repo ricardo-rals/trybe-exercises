734596f538312808c846f2baa45abc252272a1f1
Rals <ricardo.rals.dev@gmail.com>
2020-08-28 19:04:31 -0300

    Resolução da questão 3 e ajuste na questão 2

--- exercises/4.1/exercise_2.js
+++ exercises/4.1/exercise_2.js
@@ -1,7 +1,7 @@
 let A = 1;
 let B = 2;
 
-if(A >= B){
+if(A > B){
     console.log(A + " é maior que " + B);
 }else{
     console.log(B + " é maior que " + A);
---
+++ exercises/4.1/exercise_3.js
@@ -0,0 +1,11 @@
+let A = 1;
+let B = 5;
+let C = 3;
+
+if(A > B && A > C){
+    console.log(A + " é maior que " + B + " e " + C);
+}else if(B > A && B > C){
+    console.log(B + " é maior que " + A + " e " + C);
+}else{
+    console.log(C + " é maior que " + A + " e " + B);
+}
